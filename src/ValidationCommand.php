d759a0546334ee40083363e135a0cf74f90bef54
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-11 02:36:57 +0800

    Moved generator into its own file

--- src/ValidationCommand.php
+++ src/ValidationCommand.php
@@ -30,6 +30,7 @@ class ValidationCommand extends Command
     public function __construct()
     {
         parent::__construct();
+        $this->generator = new ValidationGenerator();
     }
 
     /**
@@ -39,38 +40,7 @@ class ValidationCommand extends Command
      */
     public function handle()
     {
-        $validationPath = app_path('Validations');
-        if (!app('files')->exists($validationPath)) {
-            app('files')->makeDirectory($validationPath);
-        }
-
-        $contents = file_get_contents(realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'stubs' . DIRECTORY_SEPARATOR . 'validation.stub'));
-        $contents = str_replace('ClassName', $this->argument('controller'), $contents);
-        $contents = str_replace('Methods', $this->methods(), $contents);
-
-        $filePath = $validationPath . DIRECTORY_SEPARATOR . $this->argument('controller') . 'Rules.php';
-        file_put_contents($filePath, $contents);
-        $this->info("$filePath written.");
-    }
-
-    public function methods()
-    {
-        $methods = '';
-        if ($this->option('resource')) {
-            $methods .= "    protected function store(\$request, \$params)
-    {
-        return [
-
-        ];
-    }
-
-    protected function update(\$request, \$params)
-    {
-        return [
-
-        ];
-    }";
-        }
-        return $methods;
+        $this->generator->generate($this->argument('controller'), $this->option('resource'));
+        $this->info("{$this->argument('controller')}Rules.php written.");
     }
 }
---
+++ src/ValidationGenerator.php
@@ -0,0 +1,42 @@
+<?php
+namespace Sands\Validation;
+
+class ValidationGenerator
+{
+    public function generate($controller, $resource = false)
+    {
+        $validationPath = app_path('Validations');
+        if (!app('files')->exists($validationPath)) {
+            app('files')->makeDirectory($validationPath);
+        }
+
+        $contents = file_get_contents(realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'stubs' . DIRECTORY_SEPARATOR . 'validation.stub'));
+        $contents = str_replace('ClassName', $controller, $contents);
+        $contents = str_replace('Methods', $this->methods($resource), $contents);
+
+        $filePath = $validationPath . DIRECTORY_SEPARATOR . $controller . 'Rules.php';
+        file_put_contents($filePath, $contents);
+
+    }
+
+    public function methods($resource = false)
+    {
+        $methods = '';
+        if ($resource) {
+            $methods .= "    protected function store(\$request, \$params)
+    {
+        return [
+
+        ];
+    }
+
+    protected function update(\$request, \$params)
+    {
+        return [
+
+        ];
+    }";
+        }
+        return $methods;
+    }
+}
